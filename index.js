1cc6de6396efd1ad01af78c3809ceb2c597c208a
alfian kan <alfiankan@alfians-MacBook-Pro.local>
2020-12-08 19:20:56 +0700

    add colomn index to parsing excell

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const excelToJson = require('convert-excel-to-json');
 //use modul wbm (module auto bulk whatsapp web sender via url)
 const wbm = require('wbm');
 //masukan path file untuk parsing ke JSON
-const result = excelToJson({sourceFile: 'bot.xlsx'});
+const result = excelToJson({
+    sourceFile: 'bot.xlsx',
+    columnToKey: {
+        A: 'phone',
+        B: 'nama'
+    }
+    });
 //cek hasil parsing xlsx
 console.log(result);
 //mulai bot -> ASYNC function
